3e27f45f4d68bc027a9c51620c3549039de0fb22
HP-PC <HP-PC@DESKTOP-IUQV5MR>
2018-05-19 15:35:32 +0700

    Add project files.

---
+++ AHeroFantasyWorld/Monster.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Map.h"
+class Monster
+{
+public:
+	Monster();
+	~Monster();
+	int monsterHP;
+	int monsterAtkDamage;
+	int monsterPositionX;
+	int monsterPositionY;
+	int MaxMonsterNumber = 10;
+	int monsterID;
+	void setMonsterHP();
+	void setMonsterPosition(int x, int y, Map &m);
+	void setMonsterID(int monID);
+	Monster(const Monster& ms) = default;
+	Monster& operator = (Monster& ms) = default;
+};
+
